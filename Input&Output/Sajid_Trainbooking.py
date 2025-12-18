318a8520963ed817af63d83b15f056ee4f2b2442
Dharshankuma <muthukumardharshan50@gmail.com>
2025-12-18 23:06:54 +0530

    commit changes

---
+++ Input&Output/Sajid_Trainbooking.py
@@ -0,0 +1,11 @@
+# Sajid was booking a train ticket from Chennai to Delhi for his family. Two of the relatives was interested in joining that journey from different 
+# places with their family members So, Sajid booked tickets for those persons also along with his family members. 
+# He wants to know the total number of tickets for this travel. Can you help him in finding the total number of passengers? 
+
+# Constraints 
+
+# Sajid has to declare three integer variables named as num1, num2, num3. 
+
+num = input()
+a,b,c = num.split(" ")
+print(int(a)+int(b)+int(c))
